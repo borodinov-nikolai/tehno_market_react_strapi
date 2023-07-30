64898fcca11a2fc3f06865859427dfe3248ad792
Nikolai <borodinov.nikolai@gmail.com>
2023-07-31 01:08:42 +0300

    commit

--- frontend/src/pages/smartphones/All.jsx
+++ frontend/src/pages/smartphones/All.jsx
@@ -32,22 +32,23 @@ const All = () => {
 
   return (
     <>
-    <div className= 'sort__dropdown d-flex mt-5 justify-content-end animate__animated animate__bounceInRight'>
-      <span className='me-4 pt-2' >Сортировка:</span>
-       <Form.Select aria-label="Default select" className='d-inline-block'  style={{width: '240px', backgroundColor: "rgb(235 235 235)"}}>
-        <option>По названию</option>
-        <option value="1">по цене(повозрастанию)</option>
-        <option value="2">по цене(по убыванию)</option>
-        <option value="3">по рейтингу(по возрастанию)</option>
-        <option value="4">по рейтингу(по убыванию)</option>
+    <Container >
+      <div className= 'sort__dropdown d-flex mt-5 justify-content-end animate__animated animate__bounceInRight'>
+        <span className='me-4 pt-2' >Сортировка:</span>
+         <Form.Select aria-label="Default select" className='d-inline-block'  style={{width: '240px', backgroundColor: "rgb(235 235 235)"}}>
+          <option>По названию</option>
+          <option value="1">по цене(повозрастанию)</option>
+          <option value="2">по цене(по убыванию)</option>
+          <option value="3">по рейтингу(по возрастанию)</option>
+          <option value="4">по рейтингу(по убыванию)</option>
       
-      </Form.Select>
-    </div>
-    <Container className='d-flex pt-5 gap-4 flex-wrap'>
-      {data.map(({id, attributes})=>{
-      return <ProductCard key={id} name={attributes.name} price={attributes.price} img={attributes.img}/>
-      })}
-
+        </Form.Select>
+      </div>
+      <Container className='d-flex pt-5 gap-4 flex-wrap'>
+        {data.map(({id, attributes})=>{
+        return <ProductCard key={id} name={attributes.name} price={attributes.price} img={attributes.img}/>
+        })}
+      </Container>
     </Container>
       </>
   )
